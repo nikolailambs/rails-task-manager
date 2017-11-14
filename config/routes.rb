1cd1348328f87eb6cf63cfe534f57e543c2e410e
nikolai <nikolai.lambsdorff@gmx.de>
2017-11-14 15:17:22 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+
+  # get '/index', to: 'tasks#index',
+
+  # get '/show', to: 'tasks#show'
+
+  # get '/new', to: 'tasks#new'
+
+  # get '/create', to: 'tasks#cretae'
+
+  # get '/edit', to: 'tasks#edit'
+
+  # get '/update', to: 'tasks#update'
+
+  # get '/destroy', to: 'tasks#destroy'
+
+  # patch '/tasks/:id', to: 'tasks#toggle', as: 'toggle'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks
+
+  root to: 'tasks#index'
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Task.create!(
+  name: "homework",
+  description: "Do your homework",
+  done: false
+  )
+Task.create!(
+  name: "feed pet",
+  description: "feed the pet of neighbour",
+  done: true
+  )
+Task.create!(
+  name: "buy food",
+  description: "go to the supermarket",
+  done: false
+  )
+
